a5791a62a1e160701f61b8ff890c23bfdf835bd8
johnyob <johnyob132@gmail.com>
2019-09-02 17:04:28 +0100

    Create ex1/PackedLong.java

---
+++ uk/ac/cam/ajo41/prejava/ex1/PackedLong.java
@@ -0,0 +1,30 @@
+package uk.ac.cam.ajo41.prejava.ex1; //TODO: replace your-crsid
+
+
+public class PackedLong {
+
+   /*
+    * Unpack and return the nth bit from the packed number at index position;
+    * position counts from zero (representing the least significant bit)
+    * up to 63 (representing the most significant bit).
+    */
+    public static boolean get(long packed, int position) {
+      // set "check" to equal 1 if the "position" bit in "packed" is set to 1
+      // you should use bitwise operators (not % or similar)
+      long check = (packed >> position) & 1L;
+
+      return (check == 1);
+   }
+
+   /*
+    * Set the nth bit in the packed number to the value given
+    * and return the new packed number
+    */
+   public static long set(long packed, int position, boolean value) {
+      if (value) {
+          return packed | (1L << position);
+      }
+       
+      return packed & ~(1L << position);
+   }
+ }
